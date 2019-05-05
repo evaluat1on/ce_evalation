3078c4af13fbcae7245d4462631a6e4939cce7fe
bombyzaza <darkzone_bomby@hotmail.co.th>
2019-05-05 16:56:47 +0700

    add filter subject

--- app/src/main/java/ce/evalution/ce_evaluation/Filter_subject.kt
+++ app/src/main/java/ce/evalution/ce_evaluation/Filter_subject.kt
@@ -2,11 +2,19 @@ package ce.evalution.ce_evaluation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_filter_subject.*
 
 class Filter_subject : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter_subject)
+
+
+
     }
 }
