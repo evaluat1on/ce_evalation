7404f9135941fa2a02c97e90dbd81601af9c46b6
Yokk1e <satthabut.o@gmail.com>
2019-05-06 23:06:42 +0700

    add intent view

--- app/src/main/java/ce/evalution/ce_evaluation/Login.kt
+++ app/src/main/java/ce/evalution/ce_evaluation/Login.kt
@@ -1,5 +1,6 @@
 package ce.evalution.ce_evaluation
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -17,5 +18,20 @@ class Login : AppCompatActivity() {
         username = findViewById(R.id.input_user) as EditText
         password = findViewById(R.id.input_password) as EditText
         login = findViewById(R.id.btn_login) as Button
+
+        login!!.setOnClickListener{
+            if( username!!.text.toString() == "admin"){
+                if(password!!.text.toString() == "1234"){
+                    val intent = Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            if( username!!.text.toString() == "teacher"){
+                if(password!!.text.toString() == "1234"){
+                    val intent = Intent(this,Menu_teacher::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/ce/evalution/ce_evaluation/Menu_teacher.kt
@@ -0,0 +1,12 @@
+package ce.evalution.ce_evaluation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu_teacher : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_teacher)
+    }
+}
