96c9af07a3d65400e190652e913800bc9c414128
Yokk1e <satthabut.o@gmail.com>
2019-05-11 20:15:05 +0700

    update manifest

--- app/src/main/java/ce/evalution/ce_evaluation/Dashboard.kt
+++ app/src/main/java/ce/evalution/ce_evaluation/Dashboard.kt
@@ -4,17 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.android.volley.RequestQueue
-import com.android.volley.toolbox.BasicNetwork
-import com.android.volley.toolbox.DiskBasedCache
-import com.android.volley.toolbox.HurlStack
 import org.json.JSONArray
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 
 import com.android.volley.Request
 
 import com.android.volley.Response
+import com.android.volley.toolbox.*
 
-import com.android.volley.toolbox.StringRequest
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -22,6 +20,9 @@ class Dashboard : AppCompatActivity() {
     val TAG = "SERVICE_BOOK"
     var requestQueue: RequestQueue? = null
     var BookList = ArrayList<Student_dash>()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
@@ -33,22 +34,23 @@ class Dashboard : AppCompatActivity() {
 // Set up the network to use HttpURLConnection as the HTTP client.
         val network = BasicNetwork(HurlStack())
 // Instantiate the RequestQueue with the cache and network. Start the queue.
-        val requestQueue = RequestQueue(cache, network).apply {
+        requestQueue = RequestQueue(cache, network).apply {
             start()
         }
-
-        val url = "http://10.80.39.17/TSP59/School/index.php/srp/eval/Dashboard/student_ajax"
+        requestQueue = Volley.newRequestQueue(this)
+        val url = "http://192.168.1.4:1235/get_year"
+        Log.i("test1150", url)
         // Request a string response from the provided URL.
-        val stringRequest = StringRequest(
-            Request.Method.GET, url,
-            Response.Listener<String> { response ->
+        val stringRequest = StringRequest(Request.Method.GET, url, Response.Listener {
+                response ->
                 // Display the first 500 characters of the response string.
                 val json = JSONArray(response)
+                Log.i("test1150", json.toString())
                 (0 until json.length()).mapTo(BookList){
-                    Student_dash(json.getJSONObject(it).getString("count_std"))
+                    Student_dash(json.getJSONObject(it).getString("rspAdY"))
                 }
                 textView.text = "Response is: ${
-                BookList[0].author
+                BookList[0].rspAdY
                 }"
 
                 // recycler_list.layoutManager = LinearLayoutManager(this)
@@ -56,7 +58,7 @@ class Dashboard : AppCompatActivity() {
 
 
             },
-            Response.ErrorListener {/* title.text = "That didn't work!" */}
+            Response.ErrorListener { response -> println("Error :" + response) }
         )
         stringRequest.tag = TAG
         // Add the request to the RequestQueue.
