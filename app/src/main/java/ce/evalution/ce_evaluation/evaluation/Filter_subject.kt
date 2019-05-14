61e3b36b0ba295d9e37397b9f3a06a5af1b4a913
bombyzaza <darkzone_bomby@hotmail.co.th>
2019-05-15 01:59:33 +0700

    sas

--- app/src/main/java/ce/evalution/ce_evaluation/evaluation/Filter_subject.kt
+++ app/src/main/java/ce/evalution/ce_evaluation/evaluation/Filter_subject.kt
@@ -7,6 +7,7 @@ import android.widget.Spinner
 import ce.evalution.ce_evaluation.Filter.Year
 import ce.evalution.ce_evaluation.R
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import kotlinx.android.synthetic.main.activity_filter_subject.*
@@ -14,13 +15,14 @@ import org.json.JSONArray
 
 class Filter_subject : AppCompatActivity() {
     var Year = ArrayList<Year>()
-
+    var requestQueue: RequestQueue? = null
+    val TAG = "SERVICE_BOOK"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter_subject)
 
 
-        val url = "http://192.168.1.13:1235/get_year"
+        val url = "http://10.80.83.187:1234/get_year"
         val Year_data = StringRequest(
             Request.Method.GET, url,
             Response.Listener<String> { response ->
@@ -32,8 +34,14 @@ class Filter_subject : AppCompatActivity() {
             },
             Response.ErrorListener {/* title.text = "That didn't work!" */}
         )
+        Year_data.tag = TAG
+        // Add the request to the RequestQueue.
+        requestQueue?.add(Year_data)
 
 
-
+    }
+    override fun onStop() {
+        super.onStop()
+        requestQueue?.cancelAll(TAG)
     }
 }
